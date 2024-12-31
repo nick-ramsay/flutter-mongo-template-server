99607fa443769d43378f914c06eadd4502e8435e
Nick Ramsay <NickRamsay1989@gmail.com>
2024-12-31 16:41:24 +1100

    Updated server to match react-mongo-template

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const mongoURI = process.env.mongo_uri;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const messageSchema = new mongoose.Schema({
+    message: { type: String },
+    created_date: {type: Date} 
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+app.get('/messages', async (req, res) => {
+    const messages = await Message.find();
+    res.json(messages);
+});
+
+app.post('/messages', async (req, res) => {
+    const message = new Message(req.body);
+    await message.save();
+    res.json(message);
+});
+
+app.delete('/messages/:id', async (req, res) => {
+    await Message.findByIdAndDelete(req.params.id);
+    res.json({ success: true });
+});
+
+app.listen(5000, () => console.log('Server running on port 5000'));
